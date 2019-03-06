54edb0594945ab8f38558d520b76855427c2c5fb
BreakTiger <46531043+BreakTiger@users.noreply.github.com>
2019-03-06 11:20:58 +0800

    version 3-6 0.1

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,62 +4,87 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    lunbo: [
+      '/image/banner/banner1.png',
+      '/image/banner/banner2.png',
+    ],
+    navlist: [{
+      icon: '/image/nav/icon1.png',
+      nav: '综合评价'
+    }, {
+      icon: '/image/nav/icon2.png',
+      nav: '成绩查询'
+    }, {
+      icon: '/image/nav/icon3.png',
+      nav: '课表查询'
+    }, {
+      icon: '/image/nav/icon4.png',
+      nav: '就业管理'
+    }, {
+      icon: '/image/nav/icon5.png',
+      nav: '物业保修'
+    }, {
+      icon: '/image/nav/icon6.png',
+      nav: '请假管理'
+    }, {
+      icon: '/image/nav/icon7.png',
+      nav: '选课'
+    }]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    
+  onLoad: function(options) {
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-    
+  onReady: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-    
+  onShow: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-    
+  onHide: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-    
+  onUnload: function() {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-    
+  onPullDownRefresh: function() {
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-    
+  onReachBottom: function() {
+
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-    
+  onShareAppMessage: function() {
+
   }
 })
