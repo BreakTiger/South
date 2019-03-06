775d57b2d67535e74fe23cc6d0de4230f0f05d05
BreakTiger <46531043+BreakTiger@users.noreply.github.com>
2019-03-06 12:38:01 +0800

    version 3-6 0.1.1

--- pages/information/information.js
+++ pages/information/information.js
@@ -5,62 +5,98 @@ Page({
    * 页面的初始数据
    */
   data: {
+    choose1: 1,
+    choose2: 0
 
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
+
+  },
+
+
+  // 切换事件
+  seletcInfo: function() {
+    let that = this
+    // 获取在data中的数值
+    var choose = that.data.choose1
+    console.log('当前状态：',choose);
+    // 未选中时，为0
+    if (choose==0){
+      that.setData({
+        choose1: 1,
+        choose2: 0
+      })
+    }
+   
+  },
+
+  seletcNotice: function() {
+    let that = this
+    // 获取在data中的数值
+    var choose = that.data.choose2
+    console.log('当前状态：',choose);
+    // 为选中时，为0
+    if(choose==0){
+      that.setData({
+        choose1: 0,
+        choose2: 1
+      })
+    }
+
+    
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
